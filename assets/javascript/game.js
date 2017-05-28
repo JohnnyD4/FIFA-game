5973a8da470bc0b0cd333f336ed3549379c4fb38
John Davis <johnsdavis95@gmail.com>
2017-05-28 10:27:52 -0400

    Added button and game is working in console log so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){ 
 	console.log("USA");
-
+	
+	 $("#playButton").hide();
 
 	//contrcutor functions
 
@@ -51,49 +52,39 @@ $(document).ready(function(){
 
 
 		$(teams[i].div).append(teams[i].image)
-		console.log(teams[i].name)
+		// console.log(teams[i].name)
 	}
 
-// var sources = ["assets/images/real.png", "assets/images/monaco.png", "assets/images/atletico.png", "assets/images/juventus.png"];
 
-// // function setUp(){}
-
-// for (var i = 0; i < sources.length; i++) {
-
-// 	var images = $("<img>");
-// 	images.attr({
-
-// 		"src": sources[i],
-// 		"width": "180px",
-		
-
-// 	});
-
-// 	$("#24").append(images[i]+1);
-// 	$("#25").append(images[i]);
-
-// }
-	// $("#23").append(images[0]);
-	// $("#24").append(images[1]);
-	// $("#33").append(images[2]);
-	// $("#34").append(images[3]);
-	var userTeam = 0;
+	var userTeam = "";
 	var pickMadrid = $("#madridPick");
 	var pickMonaco = $("#monacoPick");
 	var pickJuventus = $("#juventusPick");
 	var pickAthletico = $("#athleticoPick");
-	var randomSpot;
+	var goalsMadrid = Math.floor(Math.random() * 5) + 2;
+	var goalsMonaco = Math.floor(Math.random() * 4) + 1;
+	var goalsAthletico = Math.floor(Math.random() * 4) + 2;
+	var goalsJuventus = Math.floor(Math.random() * 5) + 2;
+	var finalsTeam1;
+	var finalsTeam2;
+	
+	
 
 	$("#madridPick").on("click", function() {
 		
-		userTeam = $(this).attr("name");
+		 userTeam = $(this).attr("data-name");
 		console.log("You picked " + "Real Madrid");
-
+		// (userTeam).append(pickMadrid);
+		console.log(userTeam);
+		
 		// pickMadrid = $("#madridPick");
 		$("#21").append(pickMadrid);
-		$("#22").append(pickMonaco);
-		$("#25").append(pickAthletico);
+		$("#25").append(pickMonaco);
+		$("#22").append(pickAthletico);
 		$("#26").append(pickJuventus);
+		
+		// console.log("Real Madrid scored " + goalsMadrid + " goals");
+		// console.log("Athletico Madrid scored " + goalsAthletico + " goals");
 	})
 
 	$("#monacoPick").on("click", function() {
@@ -101,13 +92,15 @@ $(document).ready(function(){
 		userTeam = $(this).attr("name");
 		console.log("You picked " + "Monaco");
 
+		
 		// pickMonaco = $("#monacoPick");
 		$("#21").append(pickMonaco);
-
-		// Lets see if this shit works lol
-		$("#22").append(pickMadrid);
+		$("#26").append(pickMadrid);
 		$("#25").append(pickAthletico);
-		$("#26").append(pickJuventus);
+		$("#22").append(pickJuventus);
+
+		// console.log("Monaco scored " + goalsMonaco + " goals");
+		// console.log("Juventus scored " + goalsJuventus + " goals");
 
 	})
 
@@ -116,6 +109,7 @@ $(document).ready(function(){
 		userTeam = $(this).attr("name");
 		console.log("You picked " + "Athletico Madrid");
 
+		
 		// pickAthletico = $("#athleticoPick");
 		$("#21").append(pickAthletico);
 		$("#22").append(pickMadrid);
@@ -128,18 +122,50 @@ $(document).ready(function(){
 		userTeam = $(this).attr("name");
 		console.log("You picked " + "Juventus");
 
+		
 		// pickJuventus = $("#juventusPick");
 		$("#21").append(pickJuventus);
-		$("#22").append(pickMadrid);
-		$("#25").append(pickAthletico);
+		$("#25").append(pickMadrid);
+		$("#22").append(pickAthletico);
 		$("#26").append(pickMonaco);
 	})
 
-	if (userTeam === "Real Madrid") {
-		console.log("It worked");
+	$("#playButton").on("click", function() {
+		console.log("Real Madrid scored " + goalsMadrid + " goals");
+		console.log("Athletico Madrid scored " + goalsAthletico + " goals");
+
+		console.log("Monaco scored " + goalsMonaco + " goals");
+		console.log("Juventus scored " + goalsJuventus + " goals");
+	
+	if (goalsMadrid > goalsAthletico) {
+		$(".madWin").append("Real Madrid wins!");
+		console.log("Real Madrid wins!");
+		// (finalsTeam1).append(pickMadrid);
+		// $("#33").append(finalsTeam1);
+
 	} else {
-		console.log("Didnt work");
+		console.log("Athletico Madrid wins!");
+		// (finalsTeam1).append(pickAthletico);
+		// $("#33").append(finalsTeam1);
 	}
+
+	if (goalsMonaco > goalsJuventus) {
+		console.log("Monaco wins!");
+		// (finalsTeam2).append(pickMonaco);
+	} else {
+		console.log("Juventus wins!");
+		// (finalsTeam2).append(pickJuventus);
+	}
+
+	})
+
+	$(".teams").on("click", function() {
+		$("#pickTeam").hide();
+		$("#playButton").show();
+
+	})
+
+
 
 });
 
